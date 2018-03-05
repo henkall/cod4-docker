ffb2b6dadfb680523c1e6e4182a30fd7e20673ef
Henrik S Nielsen <1741035+henkall@users.noreply.github.com>
2018-03-05 23:16:22 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,40 @@ if [ ! -f cod4x18_dedrun ]; then
 else
     echo "cod4x18_dedrun found, not downloading anything."
 fi
-if [[ -z "${ARGS}" ]]; then
-    ARGS="+set net_port 28963 +map mp_killhouse"
+if [ "$(ls -A mods)" ]; then
+     echo "Mods Good"
+else
+    echo "ERROR Mods is Empty"
 fi
-su server -c ./cod4x18_dedrun "$ARGS"
+if [ "$(ls -A usermaps)" ]; then
+     echo "Usermaps Good"
+else
+    echo "ERROR Usermaps is Empty"
+fi
+if [ "$(ls -A zone)" ]; then
+     echo "Zone is Good"
+else
+    echo "ERROR Zone is"
+fi
+echo "Setting port"
+if [[ -z "${PORT}" ]]; then
+  echo "The PORT variable is empty."
+  PORT="28960"
+fi
+echo "Setting MAP"
+if [[ -z "${MAP}" ]]; then
+  echo "The MAP variable is empty."
+  MAP="+map_rotate"
+fi
+echo "Checking if READY"
+if [[ ! -z "${READY}" ]]; then
+	echo "Config is Ready"
+	if [[ ! -z "${MODNAME}" ]]; then
+		echo "Mod enabled (using $MODNAME mod)"
+		./cod4x18_dedrun "+set net_port $PORT" "+set fs_game mods/$MODNAME" "$EXTRA" "+exec server.cfg" "$MAP"
+	else
+		echo "Not using Mod"
+		./cod4x18_dedrun "+set net_port $PORT" "$EXTRA" "+exec server.cfg" "$MAP"
+	fi
+
+fi
